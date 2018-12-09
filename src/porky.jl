f34d4b853e0e9bad9a7772f7a2812526dbd6c54c
German <pbarletta@gmail.com>
2018-12-09 12:22:01 -0300

    Updated porky. Still some work to be done, is a pretty awful piece of code.

--- src/porky.jl
+++ src/porky.jl
@@ -3,67 +3,10 @@
 ###############   utility to displace a PDB along a vector   ##################
 #   code by pgbarletta
 #########################################################
-# Safety pig included:
-#
-#    _._ _..._ .-',     _.._(`))
-#   '-. `     '  /-._.-'    ',/
-#      )         \            '.
-#     / _    _    |             \
-#    |  a    a    /              |
-#    \   .-.                     ;
-#     '-('' ).-'       ,'       ;
-#        '-;           |      .'
-#           \           \    /
-#           | 7  .__  _.-\   \
-#           | |  |  ``/  /`  /
-#          /,_|  |   /,_/   /
-#             /,_/      '`-'
-###############################################################################
-using Chemfiles
+using Chemfiles, JUMD
 using StaticArrays
 using ArgParse, LinearAlgebra, DelimitedFiles
-##########
-# functions
-##########
-function read_ptraj_modes(filename, nmodes::Int64=0, norma::Bool=true)
-    modes_text = readdlm(filename, skipstart=0, skipblanks=true, comments=true,
-        comment_char='*')
 
-    if nmodes == 0
-        nmodes = modes_text[1, 5]
-    end
-    modes_elements = modes_text[2, 1]
-
-    ncoords = convert(Int64, modes_elements)
-    lines = ceil(Int64, ncoords/7)
-    rest = convert(Int64, ncoords % 7)
-
-    eval = Array{Float64}(undef, nmodes);
-    mode = Array{Float64}(undef, ncoords, nmodes);
-    temp1 = Array{Float64}(undef, ncoords, 1);
-    temp2 = Array{Float64}(undef, ncoords+(7-rest));
-
-    j=lines + 1 + 2 # 1 p/ q lea la prox linea 2 por el header
-    for i=1:nmodes
-        eval[i] = modes_text[j, 2]
-        temp = permutedims(modes_text[(j+1):(lines+j), :], [2, 1])
-        temp2 = reshape(temp, ncoords+(7-rest))
-        for k=(rest+1):7
-            pop!(temp2)
-        end
-        mode[:, i] = temp2
-        j = j + lines + 1
-    end
-
-    if norma == true
-        for i=1:nmodes
-            mode[: ,i] = mode[:, i] / norm(mode[:, i])
-        end
-    end
-
-    return mode, eval
-end
-#########
 function displaceAA(in_frm, aa, aa_3, in_vec)
     # Preparo variables
     in_top = Topology(in_frm)
@@ -73,10 +16,10 @@ function displaceAA(in_frm, aa, aa_3, in_vec)
     # Determino orden de residuos (hay q actualizar el Julia Chemfiles)
     tmp = Array{Int64}(undef, aa)
     ids = Array{Int64}(undef, aa)
-    [ ids[i+1] = convert(Int64, id((Residue(in_top, i)))) for i = 0:aa-1 ]
+    [ ids[i + 1] = convert(Int64, id((Residue(in_top, i)))) for i = 0:aa - 1 ]
     idx = sortperm(ids)
     # Determino el nro de atomos de c/ aminoácido
-    [ tmp[i+1] = size(Residue(in_top, i)) for i = 0:aa-1 ]
+    [ tmp[i + 1] = size(Residue(in_top, i)) for i = 0:aa - 1 ]
     natom_aa = tmp[idx]
 
     # Paso el vector columna de tamaño 1xaa_3 a 3xaa
@@ -86,12 +29,12 @@ function displaceAA(in_frm, aa, aa_3, in_vec)
     cursor = 0
     for i = 1:aa
         if i == 1
-            sum_mat[:, 1:natom_aa[i]] = repmat(vector[:, 1], 1, natom_aa[i])
+            sum_mat[:, 1:natom_aa[i]] = repeat(vector[:, 1], 1, natom_aa[i])
             cursor = natom_aa[i]
             continue
         end
-        rango = collect(cursor+1:cursor + natom_aa[i])
-        sum_mat[:, rango] = repmat(vector[:, i], 1, natom_aa[i])
+        rango = collect(cursor + 1:cursor + natom_aa[i])
+        sum_mat[:, rango] = repeat(vector[:, i], 1, natom_aa[i])
         cursor += natom_aa[i]
     end
 
@@ -102,47 +45,42 @@ function displaceAA(in_frm, aa, aa_3, in_vec)
     # Tengo q hacer esto por ahora. Hasta q arreglemos Chemfiles.
     for i = 1:size(in_xyz)[1]
         for j = 1:size(in_xyz)[2]
-            out_xyz[i, j]  = round(in_xyz[i, j] + sum_mat[i, j], 3)
+            out_xyz[i, j]  = round(in_xyz[i, j] + sum_mat[i, j], digits = 3)
         end
     end
     return out_frm
 end
-#########
 # Arg Parse settings
 s = ArgParseSettings()
 @add_arg_table s begin
     "--inpdb", "-p"
-        help = "Input PDB"
-        arg_type = String
-        required = true
+    help = "Input PDB"
+    arg_type = String
+    required = true
     "--vector", "-v"
-        help = "Input vector"
-        arg_type = String
-        required = true
+    help = "Input vector"
+    arg_type = String
+    required = true
     "--outpdb", "-o"
-        help = "Output PDB"
-        arg_type = String
-        required = true
+    help = "Output PDB"
+    arg_type = String
+    required = true
     "--multiplier", "-m"
-        help = "Constant to scale the normalized vector. Defaults to 1"
-        arg_type = Float64
-        default = 1.
+    help = "Constant to scale the normalized vector. Defaults to 1"
+    arg_type = Float64
+    default = 1.
     "--index", "-i"
-        help = "Mode number when reading cpptraj PCA modes"
-        arg_type = Int
-        default = 0
-    "--script"
-        help = "Only write output PDB"
-        action = :store_true
+    help = "Mode number when reading cpptraj PCA modes"
+    arg_type = Int
+    default = 0
+    "--script", "-s"
+    help = "Only write output PDB"
+    action = :store_true
 end
-
-##########
-# main program
-##########
 
 # Read arguments from console
 parsed_args = parse_args(ARGS, s)
-args = Array{Any, 1}(undef, 0)
+args = Array{Any,1}(undef, 0)
 for (arg, val) in parsed_args
     arg = Symbol(arg)
     @eval (($arg) = ($val))
@@ -159,7 +97,7 @@ println("INDEX          ", index)
 println("SCRIPT         ", script)
 
 # Get ready
-in_vec = Array{Float64, 1}
+in_vec = Array{Float64,1}
 # Read PDB
 const in_trj = Trajectory(inpdb)
 const in_frm = read(in_trj)
@@ -167,11 +105,9 @@ const in_top = Topology(in_frm)
 const aa = convert(Int64, count_residues(in_top))
 const aa3 = aa * 3
 
-
-in_vec = MVector{aa3, Float64}()
 if (index == 0)
     try
-        in_vec = convert(MVector{aa3, Float64}, readdlm(vector))
+        global in_vec = convert(Array{Float64,1}, readdlm(vector)[:, 1])
     catch e
         println(vector, " error:")
         error(e)
@@ -179,31 +115,30 @@ if (index == 0)
 else
     try
         in_modes, in_evals = read_ptraj_modes(vector, index)
-        in_vec = convert(MVector{aa3, Float64}, in_modes[:, index])
+        global in_vec = convert(Array{Float64,1}, in_modes[:, index])
     catch e
         println(vector, " error:")
         error(e)
     end
 end
 
-#
-in_vec = in_vec / norm(in_vec) .* multiplier
+in_vec = in_vec ./ norm(in_vec) .* multiplier
 const out_frm = displaceAA(in_frm, aa, aa3, in_vec);
 # Y guardo
 out_trj = Trajectory(outpdb, 'w')
 write(out_trj, out_frm)
 
-const script_filename = string("script_porky_", splitext(outpdb)[1] ,".py")
+const script_filename = string("script_porky_", splitext(outpdb)[1], ".py")
 # Finalmente, hago el script
 if script == true
     load = "cmd.load(\""
     f = open(script_filename, "w")
     write(f, "from pymol.cgo import *\n")
     write(f, "from pymol import cmd\n\n")
-    write(f, load, inpdb,"\")\n")
-    write(f, load, outpdb,"\")\n")
-    write(f, load,"modevectors.py\")\n")
-    write(f, "modevectors(\"", inpdb[1:end-4], "\", \"", outpdb[1:end-4], "\", ")
+    write(f, load, inpdb, "\")\n")
+    write(f, load, outpdb, "\")\n")
+    write(f, load, "modevectors.py\")\n")
+    write(f, "modevectors(\"", inpdb[1:end - 4], "\", \"", outpdb[1:end - 4], "\", ")
     write(f, "outname=\"modevectors\", head=0.5, tail=0.3, headrgb = \"1.0, 1.0, 0.0\", tailrgb = \"1.0, 1.0, 0.0\", cutoff=3.0)")
     close(f)
 end
